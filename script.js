c4964c03aad01b9d951970131c078d208201af45
leolpaz <leonardolpaz95@gmail.com>
2021-09-13 17:35:29 -0300

    Fix linter errors about code formatting

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-var form = document.getElementById('bookForm')
-var bookArray = JSON.parse(localStorage.getItem("bookArray") || "[]");
-var listObj = document.getElementById('bookList')
-form.addEventListener("submit", AddBook)
+/* eslint-disable no-use-before-define */
+
+const form = document.getElementById('bookForm');
+const bookArray = JSON.parse(localStorage.getItem('bookArray') || '[]');
+const listObj = document.getElementById('bookList');
+
+form.addEventListener('submit', AddBook);
 
 function AddBook(event) {
-    event.preventDefault()
-    var title = document.getElementById('title').value;
-    var author = document.getElementById('author').value;
-    title = {
-        book: title,
-        author: author
-    }
-    title = title;
-    author = author;
-    bookArray.push(title)
-    localStorage.setItem("bookArray", JSON.stringify(bookArray))
-    ListPopulate();
-};
-
-function ListPopulate() {
-    listObj.innerHTML = '';
-    bookArray.forEach((bookElement, index) => {
-        listObj.innerHTML += `<li class='text-center '>Book Name: ${bookElement.book} Book Author: ${bookElement.author} <br> <button type='button' value="${index}" class='remove-btn btn btn-secondary text-center mt-2'>Remove</button></li><br>`
-    });
-    ButtonListener()
-};
-
-function ButtonListener() {
-    var buttonArray = Array.from(document.getElementsByClassName("remove-btn"))
-    buttonArray.forEach((button) => {
-        button.addEventListener("click", RemoveBook)
-    })
-};
-
-function RemoveBook(event) {
-    var bookValue = event.target.value
-    bookArray.splice(bookValue, 1)
-    localStorage.setItem("bookArray", JSON.stringify(bookArray))
-    ListPopulate()
+  event.preventDefault();
+  let title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  title = {
+    book: title,
+    author,
+  };
+  bookArray.push(title);
+  localStorage.setItem('bookArray', JSON.stringify(bookArray));
+  ListPopulate();
 }
 
-ListPopulate()
+function ListPopulate() {
+  listObj.innerHTML = '';
+  bookArray.forEach((bookElement, index) => {
+    listObj.innerHTML += `<li class='text-center '>Book Name: ${bookElement.book} Book Author: ${bookElement.author} <br> <button type='button' value="${index}" class='remove-btn btn btn-secondary text-center mt-2'>Remove</button></li><br>`;
+  });
+  ButtonListener();
+}
+
+function ButtonListener() {
+  const buttonArray = Array.from(document.getElementsByClassName('remove-btn'));
+  buttonArray.forEach((button) => {
+    button.addEventListener('click', RemoveBook);
+  });
+}
+
+function RemoveBook(event) {
+  const bookValue = event.target.value;
+  bookArray.splice(bookValue, 1);
+  localStorage.setItem('bookArray', JSON.stringify(bookArray));
+  ListPopulate();
+}
+
+ListPopulate();
