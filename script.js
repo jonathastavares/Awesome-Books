89be041fe7c5c839167bc8b4e566c0beef43b63f
leolpaz <leonardolpaz95@gmail.com>
2021-09-13 16:44:55 -0300

    Add remove book function and local storage persistence

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var form = document.getElementById('bookForm')
-var bookArray = []
+var bookArray = JSON.parse(localStorage.getItem("bookArray") || "[]");
 var listObj = document.getElementById('bookList')
 form.addEventListener("submit", AddBook)
 
@@ -7,23 +7,37 @@ function AddBook(event) {
     event.preventDefault()
     var title = document.getElementById('title').value;
     var author = document.getElementById('author').value;
-    title = {book: title, author: author}
-    this.title = title;
-    this.author = author;
+    title = {
+        book: title,
+        author: author
+    }
+    title = title;
+    author = author;
     bookArray.push(title)
-    console.log(bookArray);
+    localStorage.setItem("bookArray", JSON.stringify(bookArray))
     ListPopulate();
 };
 
 function ListPopulate() {
     listObj.innerHTML = '';
-    bookArray.forEach((bookElement) => {
-        listObj.innerHTML += `<li class='text-center'>Book Name: ${bookElement.book} Book Author: ${bookElement.author}</li><button type='button' class='btn btn-secondary'>Remove</button><br>`
-        
+    bookArray.forEach((bookElement, index) => {
+        listObj.innerHTML += `<li class='text-center '>Book Name: ${bookElement.book} Book Author: ${bookElement.author} <br> <button type='button' value="${index}" class='remove-btn btn btn-secondary text-center mt-2'>Remove</button></li><br>`
     });
+    ButtonListener()
 };
 
-function RemoveBook(index) {
-    delete bookArray[index];
-    console.log(bookArray);
+function ButtonListener() {
+    var buttonArray = Array.from(document.getElementsByClassName("remove-btn"))
+    buttonArray.forEach((button) => {
+        button.addEventListener("click", RemoveBook)
+    })
 };
+
+function RemoveBook(event) {
+    var bookValue = event.target.value
+    bookArray.splice(bookValue, 1)
+    localStorage.setItem("bookArray", JSON.stringify(bookArray))
+    ListPopulate()
+}
+
+ListPopulate()
