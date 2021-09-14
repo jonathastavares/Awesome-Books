b87e579b0f04ffba3f5eb04306767c57aacc35ed
leolpaz <leonardolpaz95@gmail.com>
2021-09-14 16:41:44 -0300

    Fix linter errors about code formatiing

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+/* eslint-disable max-classes-per-file */
 const form = document.getElementById('bookForm');
-const bookList = document.getElementById('bookList')
+const bookList = document.getElementById('bookList');
 
 class Book {
   constructor(title, author) {
@@ -10,21 +11,21 @@ class Book {
 
 class Save {
   static dataArray() {
-    let bookArray;
-    bookArray = JSON.parse(localStorage.getItem('bookArray') || '[]');
-    return bookArray
+    const bookArray = JSON.parse(localStorage.getItem('bookArray') || '[]');
+    return bookArray;
   }
+
   static saveBook(book) {
-    const bookArray = Save.dataArray()
-    bookArray.push(book)
+    const bookArray = Save.dataArray();
+    bookArray.push(book);
     localStorage.setItem('bookArray', JSON.stringify(bookArray));
-    console.log(bookArray)
   }
+
   static removeBook(event) {
-    const removeArr = Save.dataArray()
-    if (event.type == "button") {
+    const removeArr = Save.dataArray();
+    if (event.type === 'button') {
       removeArr.splice(event.value, 1);
-      localStorage.setItem('bookArray', JSON.stringify(removeArr));      
+      localStorage.setItem('bookArray', JSON.stringify(removeArr));
     }
   }
 }
@@ -32,14 +33,14 @@ class Save {
 class Load {
   static listPopulate() {
     const listObj = document.getElementById('bookList');
-      listObj.innerHTML = '';
-      Save.dataArray().forEach((bookElement, index) => {
+    listObj.innerHTML = '';
+    Save.dataArray().forEach((bookElement, index) => {
       listObj.innerHTML += `<li class='text-center '>Book Name: ${bookElement.title} Book Author: ${bookElement.author} <br> <button type='button' value="${index}" class='remove-btn btn btn-secondary text-center mt-2'>Remove</button></li><br>`;
     });
   }
 }
 
-document.addEventListener('DOMContentLoaded', Load.listPopulate())
+document.addEventListener('DOMContentLoaded', Load.listPopulate());
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -47,18 +48,17 @@ form.addEventListener('submit', (event) => {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
 
-  const book = new Book(title, author)
+  const book = new Book(title, author);
 
-  Save.saveBook(book)
-  Load.listPopulate()
-  form.reset()
+  Save.saveBook(book);
+  Load.listPopulate();
+  form.reset();
 });
 
 bookList.addEventListener('click', (event) => {
-  Save.removeBook(event.target)
+  Save.removeBook(event.target);
 
-  form.reset()
+  form.reset();
 
-  Load.listPopulate()
-})
-
+  Load.listPopulate();
+});
