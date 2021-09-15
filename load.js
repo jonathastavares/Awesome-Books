7b5231ed2acaad22334a95cc8e92b9829089364f
Jonathas Tavares <jonathastavares@id.uff.br>
2021-09-15 19:42:44 -0300

    Fix style for form

--- load.js
+++ load.js
@@ -6,7 +6,7 @@ class Load {
     const container = document.getElementById('table-container');
     listObj.innerHTML = '';
     Save.dataArray().forEach((bookElement, index) => {
-      listObj.innerHTML += `<tr><td class='d-flex justify-content-between align-items-center'><p class='no-margin'><span>'${bookElement.title}'</span> by ${bookElement.author}</p><button type='button' value="${index}" class='remove-btn text-center'>Remove</button></td></tr>`;
+      listObj.innerHTML += `<tr><td class='d-flex justify-content-between align-items-center'><p class='no-margin'><span>'${bookElement.title}'</span> by ${bookElement.author}</p><button type='button' value="${index}" class='shadow border-dark remove-btn text-center'>Remove</button></td></tr>`;
     });
     if (listObj.innerHTML === '') {
       container.classList.add('d-none');
