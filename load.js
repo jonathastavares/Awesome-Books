04774fef5c0de6ef873a9dda5f15b0a5463ecbf6
leolpaz <leonardolpaz95@gmail.com>
2021-09-15 19:26:59 -0300

    Corrected the table to it's visual specifications

--- load.js
+++ load.js
@@ -6,7 +6,7 @@ class Load {
     const container = document.getElementById('table-container');
     listObj.innerHTML = '';
     Save.dataArray().forEach((bookElement, index) => {
-      listObj.innerHTML += `<tr><th scope="row" class='border border-secondary'>${index + 1}</th><td class='d-flex justify-content-between align-items-center'><p class='no-margin'><span class='bold-text'>'${bookElement.title}'</span> by ${bookElement.author}</p><button type='button' value="${index}" class='remove-btn btn btn-secondary text-center'>X</button></td></tr>`;
+      listObj.innerHTML += `<tr><td class='d-flex justify-content-between align-items-center'><p class='no-margin'><span>'${bookElement.title}'</span> by ${bookElement.author}</p><button type='button' value="${index}" class='remove-btn text-center'>Remove</button></td></tr>`;
     });
     if (listObj.innerHTML === '') {
       container.classList.add('d-none');
