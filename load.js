f3bb278324b54047f24e2247b7a9984c7922adcb
leolpaz <leonardolpaz95@gmail.com>
2021-09-15 13:42:39 -0300

    Fix small issue with table, and make table disappear from the DOM when empty

--- load.js
+++ load.js
@@ -3,9 +3,15 @@
 class Load {
   static listPopulate() {
     const listObj = document.getElementById('bookList');
+    const container = document.getElementById('table-container');
     listObj.innerHTML = '';
     Save.dataArray().forEach((bookElement, index) => {
-      listObj.innerHTML += `<tr><th scope="row" class='align-middle border border-secondary number-height'>${index + 1}</th><td class='d-flex justify-content-between align-items-center'><p class='no-margin'><span class='bold-text'>'${bookElement.title}'</span> by ${bookElement.author}</p><button type='button' value="${index}" class='remove-btn btn btn-secondary text-center'>X</button></td></tr>`;
+      listObj.innerHTML += `<tr><th scope="row" class='border border-secondary'>${index + 1}</th><td class='d-flex justify-content-between align-items-center'><p class='no-margin'><span class='bold-text'>'${bookElement.title}'</span> by ${bookElement.author}</p><button type='button' value="${index}" class='remove-btn btn btn-secondary text-center'>X</button></td></tr>`;
     });
+    if (listObj.innerHTML === '') {
+      container.classList.add('d-none');
+    } else {
+      container.classList.remove('d-none');
+    }
   }
 }
